bcdea9b98244fd31190230a05da88f94907e5b08
Erik Vinicius Pereira <erikvinipereira@gmail.com>
2025-04-02 20:48:03 -0300

    UsingException
    Suding of how work the command Raise Exception

---
+++ error_handling/UsingException.py
@@ -0,0 +1,5 @@
+num = -10
+
+if num < 0:
+    raise Exception('Error the number is negative')
+#using Exception in this case, This end the programm and shows de Exception mensagem below the common error mensagem.
